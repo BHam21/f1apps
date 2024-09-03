ac0d0eabbb5f2a5189b28e81538f62e2daf0f62f
Brett Hammit <bhammit21@gmail.com>
2024-09-03 00:13:59 -0500

    Update RaceStrategyApp.py

--- RaceStrategyApp.py
+++ RaceStrategyApp.py
@@ -50,6 +50,18 @@ plt.rcParams['font.family'] = 'serif'
 plt.style.use('dark_background')
 
 compound_color = {'SOFT': '#FF0000', 'MEDIUM': '#FFEF00', 'HARD': '#E5E4E2', 'INTERMEDIATE': '#008000', 'WET': '#0000FF', 'UNKNOWN': '#000000'}
+# Handle any missing compounds by adding them with random colors
+unique_compounds = laps['Compound'].unique()
+for compound in unique_compounds:
+    if compound not in compound_color:
+        # Generate a random color in hex format
+        compound_color[compound] = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+
+# Replace NaN compounds with a default or random color (optional)
+if 'nan' in compound_color:
+    compound_color['nan'] = '#808080'  # Grey or any preferred color
+
+laps['Compound_Color'] = laps['Compound'].map(compound_color)
 laps['Compound_Color'] = laps['Compound'].map(compound_color)
 
 fig, ax = plt.subplots()
